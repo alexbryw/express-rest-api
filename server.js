3f8876bc3e44f6f2855910b77c3e84fc8ba15a86
alexbryw <alexbryw@gmail.com>
2020-04-09 15:27:34 +0200

    Added a GET endpoint /api/users, that retuns all users.

--- server.js
+++ server.js
@@ -4,9 +4,20 @@ const port = 3000
 const Users = require('./Users')
 
 const users = new Users()
+
 //Static will send all files requested from 'public' folder.
 app.use(express.static('public'))
+app.use(express.json())
 
-console.log(users.getUserList())
+//Sends back all users.
+app.get('/api/users', function(req, res){
+    res.json(users.getUserList())
+})
+
+//Add a new user.
+
+//Update an existing users information
+
+//Remove a user
 
 app.listen(port, () => {console.log('Server listening on http://localhost:' + port)})
